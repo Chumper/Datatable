f2fa301e599acd34012f35d99ba3404b40b4ab98
Saiful Islam <saaifulislam@gmail.com>
2014-10-14 02:44:14 +0600

    setOptions Method Fix
    setOptions Method fix

--- src/views/javascript.blade.php
+++ src/views/javascript.blade.php
@@ -4,7 +4,17 @@
         oTable = jQuery('#{{ $id }}').dataTable({
 
         @foreach ($options as $k => $o)
-            {{ json_encode($k) }}: {{ json_encode($o) }},
+            {{ json_encode($k) }}: @if(!is_array($o)) @if(preg_match("/function/", $o)) {{ $o }} @else {{ json_encode($o) }}, @endif
+                @else
+
+                [{
+                    @foreach ($o as $x => $r) 
+                    {{ json_encode($x) }}: @if(is_array($r)) {{ json_encode($r) }}, @elseif(preg_match("/function/", $r)) {{ $r }}, @else {{ json_encode($r) }} @endif
+                    
+                    @endforeach
+                }],
+                @endif
+
         @endforeach
 
         @foreach ($callbacks as $k => $o)
