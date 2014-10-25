1d399385676ad04efcdf765809f2be6fb38a4909
Rene Bartkowiak <bart@users.noreply.github.com>
2014-10-25 14:03:27 +0200

    Updated curly-braces to work with L5

--- src/views/javascript.blade.php
+++ src/views/javascript.blade.php
@@ -1,14 +1,14 @@
 <script type="text/javascript">
     jQuery(document).ready(function(){
         // dynamic table
-        oTable = jQuery('#{{ $id }}').dataTable({
+        oTable = jQuery('#{!! $id !!}').dataTable({
 
         @foreach ($options as $k => $o)
-            {{ json_encode($k) }}: @if(!is_array($o)) @if(preg_match("/function/", $o)) {{ $o }} @else {{ json_encode($o) }}, @endif
+            {!! json_encode($k) !!}: @if(!is_array($o)) @if(preg_match("/function/", $o)) {!! $o !!} @else {!! json_encode($o) !!}, @endif
                 @else
                 [{
                     @foreach ($o as $x => $r) 
-                    {{ json_encode($x) }}: @if(is_array($r)) {{ json_encode($r) }}, @elseif(preg_match("/function/", $r)) {{ $r }}, @else {{ json_encode($r) }} @endif
+                    {!! json_encode($x) !!}: @if(is_array($r)) {!! json_encode($r) !!}, @elseif(preg_match("/function/", $r)) {!! $r !!}, @else {!! json_encode($r) !!} @endif
                     @endforeach
                 }],
                 @endif
@@ -16,7 +16,7 @@
         @endforeach
 
         @foreach ($callbacks as $k => $o)
-            {{ json_encode($k) }}: {{ $o }},
+            {!! json_encode($k) !!}: {!! $o !!},
         @endforeach
 
         });
