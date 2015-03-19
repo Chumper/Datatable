15c4f245db284e7f29e8f4eb479da9d7ccb521ff
Todd Blanchard <tblanchard@mac.com>
2015-03-19 16:22:38 -0700

    fixed typo

--- src/views/javascript.blade.php
+++ src/views/javascript.blade.php
@@ -1,7 +1,7 @@
 <script type="text/javascript">
     jQuery(document).ready(function(){
         // dynamic table
-        oTable = jQuery('#{{ $id }}').dataTable({{ $optionString }});
+        oTable = jQuery('#{{ $id }}').dataTable({{ $options_string }});
     // custom values are available via $values array
     });
 </script>
