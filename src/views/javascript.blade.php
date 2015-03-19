a194b6d83b14ab469f3d18f68f203c3b3065ec03
Todd Blanchard <tblanchard@mac.com>
2015-03-19 16:05:34 -0700

    added recursive option rendering to template

--- src/views/javascript.blade.php
+++ src/views/javascript.blade.php
@@ -1,25 +1,7 @@
 <script type="text/javascript">
     jQuery(document).ready(function(){
         // dynamic table
-        oTable = jQuery('#{{ $id }}').dataTable({
-
-        @foreach ($options as $k => $o)
-            {{ json_encode($k) }}: @if(!is_array($o)) @if(preg_match("/function/", $o)) {{ $o }} @else {{ json_encode($o) }}, @endif
-                @else
-                [{
-                    @foreach ($o as $x => $r) 
-                    {{ json_encode($x) }}: @if(is_array($r)) {{ json_encode($r) }}, @elseif(preg_match("/function/", $r)) {{ $r }}, @else {{ json_encode($r) }} @endif
-                    @endforeach
-                }],
-                @endif
-
-        @endforeach
-
-        @foreach ($callbacks as $k => $o)
-            {{ json_encode($k) }}: {{ $o }},
-        @endforeach
-
-        });
+        oTable = jQuery('#{{ $id }}').dataTable({{ $optionString }});
     // custom values are available via $values array
     });
 </script>
