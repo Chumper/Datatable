10b9c5b8f9ee748498b5400a886fcf82a2c8add9
Dmitry Pupinin <dlnsk@mail.ru>
2015-03-24 20:16:20 +0600

    Fix bug. Option that is an array (with numeric keys) printing as object

--- src/views/javascript.blade.php
+++ src/views/javascript.blade.php
@@ -5,12 +5,19 @@
 
         @foreach ($options as $k => $o)
             {{ json_encode($k) }}: @if(!is_array($o)) @if(preg_match("/function/", $o)) {{ $o }}, @else {{ json_encode($o) }}, @endif
+                @elseif(key($o) === 0) {{-- if we have an array, no need to print keys --}}
+                [
+                    @foreach ($o as $r)
+                    @if(is_array($r)) {{ json_encode($r) }}, @elseif(preg_match("/function/", $r)) {{ $r }}, @else {{ json_encode($r) }}, @endif
+
+                    @endforeach
+                ],
                 @else
-                [{
+                {
                     @foreach ($o as $x => $r) 
                     {{ json_encode($x) }}: @if(is_array($r)) {{ json_encode($r) }}, @elseif(preg_match("/function/", $r)) {{ $r }}, @else {{ json_encode($r) }}, @endif
                     @endforeach
-                }],
+                },
                 @endif
 
         @endforeach
