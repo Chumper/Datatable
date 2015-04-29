d1fb60a8e459a183c6c742ef1964431dbc6ebaf6
Nguyễn Thiện Hùng <nguyenthienhung1989@gmail.com>
2015-04-29 12:33:23 +0700

    Reformat

--- src/views/javascript.blade.php
+++ src/views/javascript.blade.php
@@ -2,22 +2,27 @@
     jQuery(document).ready(function(){
         // dynamic table
         oTable = jQuery('#{{ $id }}').dataTable({
-@if ($first = true)
-@foreach ( $options as $k => $o )@if ( $first == false ),@endif
-@if (($first = false) == false &&!is_numeric($k)){{ json_encode($k); $obj_parent = true }}:@endif
-@if ( is_string($o))
-@if ( @preg_match("#^\s*function\s*\([^\)]*#", $o))
-{{ $o }}@else
-{{ json_encode($o) }}@endif
-@else
-@if (is_array($o) && ($obj = false) == false)@include(Config::get('datatable::table.options_view'), array('options' => $o))@else
-{{ json_encode($o) }}@endif 
-@endif
-@endforeach
-@endif
-@if ( $first == false ),@endif
-@foreach ($callbacks as $k => $o) {{ json_encode($k) }}: {{ $o }},
-@endforeach
-});
+            @foreach ( $options as $k => $o )
+                @if (!is_numeric($k))
+                    {{ json_encode($k); }}:
+                @endif
+                @if (is_string($o))
+                    @if ( @preg_match("#^\s*function\s*\([^\)]*#", $o))
+                        {{ $o }}
+                    @else
+                        {{ json_encode($o) }}
+                    @endif
+                @else
+                    @if (is_array($o))
+                        @include(Config::get('datatable::table.options_view'), array('options' => $o))
+                    @else
+                        {{ json_encode($o) }}
+                    @endif
+                @endif ,
+            @endforeach
+            @foreach ($callbacks as $k => $o)
+                {{ json_encode($k) }}: {{ $o }} ,
+            @endforeach
+        });
     });
 </script>
