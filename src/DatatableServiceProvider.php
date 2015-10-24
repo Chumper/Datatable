d1ba1455c673972302364166d6bfc9b37601ce29
Nils Plaschke <n.plaschke@dsfishlabs.com>
2015-10-24 16:02:53 +0200

    Added defered service provider

---
+++ src/DatatableServiceProvider.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Chumper\Datatable;
+
+use Illuminate\Support\ServiceProvider;
+
+class DatatableServiceProvider extends ServiceProvider
+{
+
+    /**
+     * Indicates if loading of the provider is deferred.
+     *
+     * @var bool
+     */
+    protected $defer = true;
+
+    /**
+     * Register the application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+}
