3ecd6096d0ca596bcd7365161985832611c326cc
Nils Plaschke <n.plaschke@dsfishlabs.com>
2015-10-25 02:14:17 +0100

    updated composer.json to include correct dependencies

--- tests/Chumper/Datatable/Composers/DTDataComposerTest.php
+++ tests/Chumper/Datatable/Composers/DTDataComposerTest.php
@@ -1,12 +1,9 @@
 <?php
 
-namespace Chumper\Datatable\Composers;
-
-
+use Chumper\Datatable\Composers\DTDataComposer;
 use Chumper\Datatable\Providers\DTProvider;
-use Mockery as m;
 
-class DTDataComposerTest extends \PHPUnit_Framework_TestCase
+class DTDataComposerTest extends PHPUnit_Framework_TestCase
 {
 
     /**
@@ -21,12 +18,12 @@ class DTDataComposerTest extends \PHPUnit_Framework_TestCase
 
     public function tearDown()
     {
-        m::close();
+        Mockery::close();
     }
 
     protected function setUp()
     {
-        $this->provider = m::mock('Chumper\Datatable\Providers\DTProvider');
+        $this->provider = Mockery::mock('Chumper\Datatable\Providers\DTProvider');
         $this->composer = new DTDataComposer($this->provider);
     }
 
